6e736892c85d4285f5cc64a04d9d1224d9d4b84b
sai-wunna <saiwunna944@gmail.com>
2024-02-12 09:06:03 +0630

    star effect and download counter updated

--- modules/main.js
+++ modules/main.js
@@ -2,6 +2,9 @@
   const _ = (await import('./dom.js')).default
   const body = _.getNode('body')
   const data_provider = _.getNodeById('data_provider')
+  const download_counter = _.getNodeById('download_counter')
+
+  _.getNode('h1').style.color = 'transparent'
 
   _.on('click', body, handleMoneyFly())
 
@@ -49,11 +52,31 @@
     }
   }
 
-  ;(function showingData() {
-    let encounter = false
+  ;(() => {
+    let turn = false
+    data_provider.style.animation = 'fadeInOut 5s infinite'
     setInterval(() => {
-      data_provider.textContent = encounter ? '2D' : '3D'
-      encounter = !encounter
-    }, 3000)
+      data_provider.textContent = turn ? '2D' : '3D'
+      turn = !turn
+    }, 5000)
+  })()
+  ;(() => {
+    let counter = 0
+    let interval = setInterval(() => {
+      if (counter > 10000) {
+        clearInterval(interval)
+        return
+      }
+      download_counter.textContent = calValue(counter)
+    })
+    function calValue(num) {
+      if (num > 999) {
+        counter += 100
+        return `${num.toString().slice(0, num.toString().length - 3)}k+`
+      } else {
+        counter++
+        return num
+      }
+    }
   })()
 })()
